ecef7f4049fd64af975b260d720a4cbad91e9fb5
Teffen Ellis (Eric) <teffen@nirri.us>
2016-01-11 03:15:37 -0800

    Flesh out stock search by name or symbol.

--- app/containers/stocks/index.js
+++ app/containers/stocks/index.js
@@ -43,18 +43,45 @@ class Stocks extends Component {
       })
   }
 
+  handleQueryChange({target: {value}}) {
+    this.setState({query: value.trim()})
+  }
+
   render() {
     if (this.state.stocks.length === 0) return this.renderPlaceholder()
 
-    const {activeStockId, stocks} = this.state
+    const {stocks, query} = this.state
+    const pattern = new RegExp(query, "i")
+    let {activeStockId} = this.state
+
+    function matchedQuery({name, symbol}) {
+      return [name, symbol].some(criteria => criteria.search(pattern) !== -1)
+    }
+
+    const visibleStocks = query.length > 0 ? stocks.filter(matchedQuery) : stocks
+
+    activeStockId = visibleStocks.length === 1 ? visibleStocks[0].id : activeStockId
 
     return <section data-component="stocks">
       <div className="stock-list">
-        {this.renderStocks()}
+        <input
+          className="field stock-search"
+          onChange={this.handleQueryChange.bind(this)}
+          placeholder="Search by symbol or name."
+          type="text"
+          value={query} />
+
+        <div className="stocks">
+          {visibleStocks.length > 0 ? visibleStocks.map((stock, index) => <Stock
+            active={stock.id === activeStockId}
+            key={index}
+            onClick={this.fetchStockDetails.bind(this, stock.id)}
+            stock={stock} />) : <div className="no-results">No results for "{query}"</div>}
+        </div>
       </div>
 
       <div className="stock-detail">
-        {activeStockId && <StockDetail stock={stocks.find(stock => stock.id === activeStockId)}/>}
+        {activeStockId && <StockDetail stock={stocks.find(stock => stock.id === activeStockId)} />}
       </div>
     </section>
   }
@@ -63,16 +90,6 @@ class Stocks extends Component {
     return <section data-component="stocks">
       Loading
     </section>
-  }
-
-  renderStocks() {
-    const {activeStockId, stocks} = this.state
-
-    return stocks.map((stock, index) => <Stock
-      active={stock.id === activeStockId}
-      key={index}
-      onClick={this.fetchStockDetails.bind(this, stock.id)}
-      stock={stock} />)
   }
 }
 
