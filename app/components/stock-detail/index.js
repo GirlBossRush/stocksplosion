545c26f6ed46af38c5b9d0d651ba5e97953afa7f
Teffen Ellis (Eric) <teffen@nirri.us>
2016-01-11 23:35:09 -0800

    Flesh out stock action labels.
    - Use function form for stock detail component.

--- app/components/stock-detail/index.js
+++ app/components/stock-detail/index.js
@@ -1,60 +1,47 @@
 import "./stock-detail.styl"
 
 import {max, min} from "lodash"
-import React, {Component} from "react"
+import React from "react"
 
-class StockDetail extends Component {
-  render() {
-    const {stock} = this.props
-    const symbolStyle = {
-      backgroundColor: `hsl(${stock.hue}, 100%, 47%)`,
-      textShadow: `1px 1px 0 hsl(${stock.hue}, 100%, 30%)`
+export default function StockDetail({stock: {hue, prices}}) {
+  const lowestValue = min(prices, price => price.value).value
+  const highestValue = max(prices, price => price.value).value
+  const baseSaturation = 47
+  let previousValue
+
+  const priceComponents = prices.map(({date, value}, index) => {
+    const delta = previousValue ? value - previousValue : 0
+    const label = {
+      [lowestValue]: "LOWEST",
+      [highestValue]: "HIGHEST"
+    }[value]
+
+    const barStyle = {
+      backgroundColor: `hsl(${hue}, 100%, ${value / highestValue * baseSaturation}%)`,
+      color: `hsl(${hue}, 100%, 90%)`,
+      flex: `0 1 ${value / highestValue * 75}%`
     }
 
-    return <section data-component="stock-detail">
-      <div className="prices">
-        {this.renderPrices()}
+    // FIXME: Side effects in a map aren't ideal.
+    previousValue = value
+
+    return <div className="price" key={index}>
+      <div className="bar" style={barStyle}>
+        <div>{date.format("YYYY-MM-DD")}</div>
+        <div>{value}</div>
       </div>
-    </section>
-  }
 
-  renderPrices() {
-    const {hue, prices} = this.props.stock
-    const lowestValue = min(prices, price => price.value).value
-    const highestValue = max(prices, price => price.value).value
-    const baseSaturation = 47
-    let previousValue
+      <div className="delta" data-delta={delta > 0 ? "positive" : "negative"}>
+        {delta.toFixed(2)}
+      </div>
 
-    return prices.map(({date, value}, index) => {
-      const delta = previousValue ? value - previousValue : 0
-      const label = {
-        [lowestValue]: "LOWEST",
-        [highestValue]: "HIGHEST"
-      }[value]
-
-      const barStyle = {
-        backgroundColor: `hsl(${hue}, 100%, ${value / highestValue * baseSaturation}%)`,
-        color: `hsl(${hue}, 100%, 90%)`,
-        width: `${value / highestValue * 100}%`
-      }
-
-      // FIXME: Side effects in a map aren't ideal.
-      previousValue = value
-
-      return <div className="price" key={index}>
-        <div className="bar" style={barStyle}>
-          <div>{date.format("YYYY-MM-DD")}</div>
-          <div>{value}</div>
-        </div>
-
-        <div className="delta" data-delta={delta > 0 ? "positive" : "negative"}>
-          {delta.toFixed(2)}
-        </div>
-
-        {label && <div className="label">{label}</div>}
+      {label && <div className="label">{label}</div>}
     </div>
-    })
-  }
-}
+  })
 
-export default StockDetail
+  return <section data-component="stock-detail">
+    <div className="prices">
+      {priceComponents}
+    </div>
+  </section>
+}
